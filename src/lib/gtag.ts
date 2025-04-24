a6371f5deb1c899e350c2c7aaad799d2b66e43b1
Rodrigo Dias <diasbass@hotmail.com>
2025-04-24 00:24:59 -0300

    fix: remover uso de any em window.gtag

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,5 +1,11 @@
 export const GA_TRACKING_ID = 'G-TY60WB1CGY'
 
+declare global {
+  interface Window {
+    gtag: (...args: unknown[]) => void
+  }
+}
+
 export const pageview = (url: string) => {
   window.gtag('config', GA_TRACKING_ID, {
     page_path: url,
